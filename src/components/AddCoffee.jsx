6d0b15f8651b9b2a0ba8669087661b7e0934e3cf
jerin-arpa <ismatjerrin3@gmail.com>
2023-10-17 12:11:06 +0600

    Send coffee data to the server and store data in Mongodb

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2'
+
 const AddCoffee = () => {
 
 
@@ -16,6 +18,28 @@ const AddCoffee = () => {
 
         const newCoffee = { name, supplier, category, quantity, taste, details, photo };
         console.log(newCoffee);
+
+
+        // send data to the server
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'User added successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+            });
     }
 
 
